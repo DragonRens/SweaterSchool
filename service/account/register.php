00ee941046d1c0f9b901468bd5c1a90ce94a8304
dragonren <dragonren@outlook.com>
2023-07-30 21:41:36 +0800

    develop

--- service/account/register.php
+++ service/account/register.php
@@ -1,24 +1,61 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once("../library/requests_format.php");
+    require_once("../library/standard.php");
+    require_once("../library/sql.php");
+
+    $seek = new seek();
     // 获取POST请求中的注册数据
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $email = $_POST["email"];
+    $username = $seek->getValue("username");
+    $password = $seek->getValue("password");
+    $email = $seek->getValue("email");
 
-    // TODO: 在此处添加注册数据的处理逻辑
-    // 可以将数据保存到数据库，进行验证等操作
+    // 判断用户名、密码和邮箱是否为空
+    if (empty($username) || empty($password) || empty($email)) {
+        $seek->response(array(
+            "success" => false,
+            "message" => "输入的数据不合法"
+        ),"application/json");
+        ab_exit();
+    }
+    // 判断用户名是否含有特殊字符（只允许字母、数字、下划线）
+    if (!preg_match('/^[a-zA-Z0-9_]+$/', $username)) {
+        $seek->response(array(
+            "success" => false,
+            "message" => "用户名只能包含字母、数字和下划线"
+        ),"application/json");
+        ab_exit();
+    }
+    // 判断邮箱格式是否正确
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $seek->response(array(
+            "success" => false,
+            "message" => "邮箱格式不正确！"
+        ),"application/json");
+        ab_exit();
+    }
+    $data_base = new DatabaseHelper();
 
-    $response = array(
-        "success" => true,
-        "message" => "注册成功！",
-        "data" => array(
-            "username" => $username,
-            "email" => $email
-        )
-    );
+    // 获取当前时间作为注册日期
+    $registration_date = date('Y-m-d H:i:s');
 
-    // 将数据转换为JSON格式并输出
-    header('Content-Type: application/json');
-    echo json_encode($response);
-}
+    // 构建插入语句
+    $insert_sql = "INSERT INTO school_users (registration_date, birthday, nickname, identity, email, phone_number) VALUES ('$registration_date', '', '', '', '$email', '')";
+
+    try {
+        // 执行插入语句
+        $data_base->query($insert_sql);
+
+        $seek->response(array(
+            "success" => true,
+            "message" => "注册成功！"
+        ), "application/json");
+        ab_exit();
+    } catch (Exception $error) {
+        $seek->response(array(
+            "success" => false,
+            "message" => "数据库错误：" . $error->getMessage()
+        ), "application/json");
+        ab_exit();
+    }
+
 ?>
--- service/library/sql.php
+++ service/library/sql.php
@@ -1,39 +1,112 @@
 <?php
+/**
+ * DatabaseHelper类用于简化与数据库的交互，封装了连接数据库和执行查询的方法
+ */
 class DatabaseHelper {
+    // 数据库连接所需的私有属性
+
+    /**
+     * 数据库服务器的主机名
+     * @var string
+     */
     private $server_name;
+
+    /**
+     * 数据库用户名
+     * @var string
+     */
     private $username;
+
+    /**
+     * 数据库密码
+     * @var string
+     */
     private $password;
+
+    /**
+     * 数据库名
+     * @var string
+     */
     private $dbname;
+
+    /**
+     * 数据库连接对象
+     * @var mysqli|null
+     */
     private $conn;
 
+    /**
+     * 构造函数，在创建DatabaseHelper对象时执行
+     */
     public function __construct() {
+        // 导入数据库配置文件
         require_once('../config/sql.php');
-        $this->server_name = $server_name;
-        $this->username = $username;
-        $this->password = $password;
-        $this->dbname = $dbname;
+        // 初始化属性
+        $this->server_name = $server_name; // 设置数据库服务器的主机名
+        $this->username = $username; // 设置数据库用户名
+        $this->password = $password; // 设置数据库密码
+        $this->dbname = $dbname; // 设置数据库名
+
+        // 连接数据库
         $this->connect();
     }
 
+    /**
+     * 私有方法，用于连接数据库
+     */
     private function connect() {
+        // 创建一个mysqli连接对象
         $this->conn = new mysqli($this->server_name, $this->username, $this->password, $this->dbname);
+
+        // 检查是否连接成功
         if ($this->conn->connect_error) {
             die("数据库连接失败: " . $this->conn->connect_error);
         }
+
+        // 设置字符集为UTF-8，防止乱码
+        $this->conn->set_charset("utf8");
     }
 
+    /**
+     * 公有方法，用于执行SQL查询语句
+     * @param string $sql 要执行的SQL查询语句
+     * @return mixed 查询结果（可能是mysqli_result对象，也可能是布尔值或其他数据类型）
+     * @throws Exception 如果查询失败，抛出异常
+     */
     public function query($sql) {
+        // 执行SQL查询，并将结果赋值给$result
         $result = $this->conn->query($sql);
-        if (!$result) {
-            die("查询错误: " . $this->conn->error);
+
+        // 检查查询是否出错
+        if ($result === false) {
+            throw new Exception("查询错误: " . $this->conn->error);
         }
+
+        // 返回查询结果
         return $result;
     }
 
     // 如果有其他常用的查询方法，可以继续在这里添加
 
+    /**
+     * 关闭数据库连接的方法
+     */
     public function close() {
-        $this->conn->close();
+        // 关闭数据库连接
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+
+    /**
+     * 获取上一次插入操作生成的自增ID
+     * @return int|null 返回自增ID，如果没有自增ID，则返回null
+     */
+    public function getLastInsertId() {
+        if ($this->conn) {
+            return $this->conn->insert_id;
+        }
+        return null;
     }
 }
 ?>
